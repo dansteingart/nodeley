4adbc683f9b173e7389a42fab0126184fe606ad0
Dan Steingart <dan@raspberrypi.(none)>
2012-10-24 17:30:02 -0400

    rewrite to handle newer express frameworks

--- expresserver.js
+++ expresserver.js
@@ -3,25 +3,30 @@ var url = require("url"); // URL Handling
 var fs = require('fs'); // Filesystem Access (writing files)
 
 var express = require('express'), //App Framework (similar to web.py abstraction)
-    app = express.createServer();
+    app = express();
 	app.use(express.bodyParser());
 	app.use(app.router);
-io = require('socket.io').listen(app); //Socket Creations
-io.set('log level', 1)
+io = require('socket.io'); //Socket Creations
 
 var serialport = require("serialport") //Serial Port Creation
 var SerialPort = require("serialport").SerialPort 
-var serialPort = new SerialPort("/dev/tty.usbserial",{baudrate:57600,parser:serialport.parsers.readline("\r")});
+var serialPort = new SerialPort("/dev/ttyUSB0",{baudrate:57600,parser:serialport.parsers.readline("\r")});
 
 app.use("/static", express.static(__dirname + '/static/'));
-app.use("/socket.io", express.static(__dirname + '/node_modules/socket.io/lib'));
+//app.use("/socket.io", express.static(__dirname + '/node_modules/socket.io/lib'));
 app.get('/', function(req, res){
 	indexer = fs.readFileSync('index.html').toString()
     res.send(indexer);
 });
 
-app.listen(4000);
-console.log('Express server started on port %s', app.address().port);
+server = http.createServer(app)
+io = io.listen(server); //Socket Creations
+io.set('log level', 1)
+
+
+porter = 4000
+server.listen(porter);
+console.log('Express server started on port %s', porter );
 
 data_log = []
 
@@ -38,7 +43,7 @@ io.sockets.on('connection', function (socket) {
   	socket.on('data_back', function (data) 
 	{
 		foo = data['data']
-		//console.log(foo)
+		console.log(foo)
 		
 		//io.sockets.emit('new_data',{'data':data})    
 		serialPort.write(foo+"\n")
